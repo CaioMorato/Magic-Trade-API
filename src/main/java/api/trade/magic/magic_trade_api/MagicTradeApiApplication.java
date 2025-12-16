b536a410c5ad15e0eed4eacba646b34a7ab11b05
Morato Dev <morato_dev@outlook.com>
2025-12-15 21:48:14 -0300

    iniciando projeto

---
+++ src/main/java/api/trade/magic/magic_trade_api/MagicTradeApiApplication.java
@@ -0,0 +1,21 @@
+package api.trade.magic.magic_trade_api;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MagicTradeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(MagicTradeApiApplication.class);
+		builder.bannerMode(Banner.Mode.OFF);
+
+		ConfigurableApplicationContext appContext = builder.context();
+
+		builder.run(args);
+	}
+
+}
---
+++ src/test/java/api/trade/magic/magic_trade_api/MagicTradeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.trade.magic.magic_trade_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicTradeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
